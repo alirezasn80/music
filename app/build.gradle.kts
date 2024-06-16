1f06658366b52260d0f1fe96c7c9060c0adc35bd
alireza <seyed.alireza.nejati.80@gmail.com>
2024-06-16 17:13:24 +0330

    release v1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "ir.flyap.music_a"
         minSdk = 21
         targetSdk = 34
-        versionCode = 101
-        versionName = "1.0.1"
+        versionCode = 102
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/ir/flyap/music_a/utill/Debuger.kt
+++ app/src/main/java/ir/flyap/music_a/utill/Debuger.kt
@@ -2,7 +2,7 @@ package ir.flyap.music_a.utill
 
 import android.util.Log
 
-const val DEBUG = false
+const val DEBUG = true
 
 fun debug(message: String?, tag: String = "AppDebug") {
     if (DEBUG)
